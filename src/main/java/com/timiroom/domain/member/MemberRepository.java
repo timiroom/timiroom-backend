7ceb3f237d9eeee0e4fac9b09340324d3fff0c6a
wjddydghks <wjddydghks0801@gmail.com>
2026-05-04 13:29:20 +0900

    MemberDetailServercie, findAllByMemberName 삭제

--- src/main/java/com/timiroom/domain/member/MemberRepository.java
+++ src/main/java/com/timiroom/domain/member/MemberRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findAllByMemberName(String memberName);
     Optional<Member> findByProviderAndProviderId(String provider, String providerId);
 }
